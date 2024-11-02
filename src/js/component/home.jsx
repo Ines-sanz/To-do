397a0bb0d163396e022a3f0a7e349c25f107b9d9
Inés Sanz <inessanzm@hotmail.es>
2024-11-02 19:03:45 +0000

    feat: first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim() !== "") {
+      setList([task, ...list]);
+      setTask("");
+    } else {
+      alert("Task cannot be empty");
+    }
+  };
+
+  const handleDelete = (index) => {
+    setList((prevList) => prevList.filter((_, i) => i !== index));
+  };
+
+  console.log(task);
+  console.log("-------->", list);
+  return (
+    <div className="container-fluid">
+      <h1 className="myTitle pt-5 pb-1">To do :</h1>
+      <div className="d-flex justify-content-center">
+        <div className="myContainer">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              onChange={(e) => setTask((prev) => (prev = e.target.value))}
+              value={task}
+              className="myInput"
+              placeholder="What needs to be done?"
+            />
+          </form>
+          <ul>
+            {list.length > 0 ? (
+              list.map((el, i) => (
+                <div className="myTasks" key={i}>
+                  {el}
+                  <span
+                    className="fa-solid fa-xmark myXMark"
+                    onClick={() => handleDelete(i)}
+                  ></span>
+                </div>
+              ))
+            ) : (
+              <div className="noTasks">No tasks left to do!</div>
+            )}
+          </ul>
+          <div className="leftTasks">
+            <p>{list.length > 0
+              ? list.length > 1
+                ? `${list.length} tasks to do`
+                : `${list.length} task to do`
+              : "No tasks."}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
